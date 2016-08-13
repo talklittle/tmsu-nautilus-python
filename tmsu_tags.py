5abc625b19ffdc48f13a6fb703a7bb0ce7bcdc1f
Andrew Shu <talklittle@gmail.com>
2016-08-13 12:06:24 -0700

    menu: add tag, dialog UI

--- tmsu_tags.py
+++ tmsu_tags.py
@@ -1,4 +1,9 @@
-from gi.repository import Nautilus, GObject
+import gi
+gi.require_version('Nautilus', '3.0')
+gi.require_version('Gtk', '3.0')
+from gi.repository import Nautilus, GObject, Gtk, Gdk
+import os
+import urllib
 
 class TmsuTagsExtension(GObject.GObject, Nautilus.ColumnProvider, Nautilus.InfoProvider, Nautilus.MenuProvider):
     def __init__(self):
@@ -19,33 +24,55 @@ class TmsuTagsExtension(GObject.GObject, Nautilus.ColumnProvider, Nautilus.InfoP
         file.add_string_attribute('block_size', str(os.stat(filename).st_blksize))
 
     def get_file_items(self, window, files):
-        top_menuitem = Nautilus.MenuItem(name='ExampleMenuProvider::Foo',
-                                         label='Foo',
-                                         tip='',
-                                         icon='')
+        top_menuitem = Nautilus.MenuItem(name='TmsuTagsExtension::TMSU',
+                                         label='TMSU',
+                                         tip='TMSU tags')
 
         submenu = Nautilus.Menu()
         top_menuitem.set_submenu(submenu)
 
-        sub_menuitem = Nautilus.MenuItem(name='ExampleMenuProvider::Bar',
-                                         label='Bar',
-                                         tip='',
-                                         icon='')
-        submenu.append_item(sub_menuitem)
+        add_tag_menuitem = Nautilus.MenuItem(name='TmsuTagsExtension::Add_Tag',
+                                             label='Add tags',
+                                             tip='Add tags')
+        add_tag_menuitem.connect('activate', self.add_tag_activate_cb, files)
+        submenu.append_item(add_tag_menuitem)
 
         return top_menuitem,
 
-    def get_background_items(self, window, file):
-        submenu = Nautilus.Menu()
-        submenu.append_item(Nautilus.MenuItem(name='ExampleMenuProvider::Bar2',
-                                         label='Bar2',
-                                         tip='',
-                                         icon=''))
+    def add_tag_activate_cb(self, menu, files):
+        win = AddTagsWindow(files)
+        win.connect("delete-event", Gtk.main_quit)
+        win.show_all()
+        Gtk.main()
 
-        menuitem = Nautilus.MenuItem(name='ExampleMenuProvider::Foo2',
-                                         label='Foo2',
-                                         tip='',
-                                         icon='')
-        menuitem.set_submenu(submenu)
+class AddTagsWindow(Gtk.Window):
+    def __init__(self, files):
+        Gtk.Window.__init__(self, title="TMSU")
+        self.set_size_request(200, 100)
+        self.set_border_width(10)
+        self.set_type_hint(Gdk.WindowTypeHint.DIALOG)
 
-        return menuitem,
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        self.add(vbox)
+
+        prompt_text = 'Add (space-separated) tags to %d file%s' % (len(files), '' if len(files)==1 else 's')
+        self.prompt_label = Gtk.Label(label=prompt_text)
+        vbox.pack_start(self.prompt_label, True, True, 0)
+
+        self.entry = Gtk.Entry()
+        self.entry.connect("activate", self.on_entry_activated)
+        vbox.pack_start(self.entry, True, True, 0)
+
+        self.button = Gtk.Button(label="Add")
+        self.button.connect("clicked", self.on_button_clicked)
+        vbox.pack_start(self.button, True, True, 0)
+
+    def on_entry_activated(self, widget):
+        self.add_tags()
+
+    def on_button_clicked(self, widget):
+        self.add_tags()
+
+    def add_tags(self):
+        tags = self.entry.get_text()
+        print(tags)
